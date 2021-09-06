c0b5e515cdfdb1a00caafc4d4151ca587d68fa05
Dzmitry Mialeshka <dmialeshka@exadel.com>
2021-09-06 23:08:46 +0300

    task01

---
+++ tasks/task01/narcissistic_numbers.py
@@ -0,0 +1,19 @@
+import math
+
+k = int(input("Please enter an integer: "))
+
+
+def get_digits_number(num: int) -> int:
+    return int(math.log(num, 10)) + 1
+
+
+def is_narcissistic_num(num: int) -> bool:
+    n: int = get_digits_number(num)
+    nth: [int] = [int(digit) ** n for digit in str(num)]
+    return num == sum(nth)
+
+
+while 1 <= k <= 1000:
+    if is_narcissistic_num(k):
+        print(f'narcissistic number {k}')
+    k += 1
