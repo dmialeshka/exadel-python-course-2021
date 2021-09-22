45d33d968e3a5913c91d3922a008c7001bea14d4
dmialesha <dmialeshka@exadel.com>
2021-09-22 17:09:09 +0300

    task05: fix task assignment

--- tasks/task05/user_factory.py
+++ tasks/task05/user_factory.py
@@ -1,5 +1,32 @@
 """
 Implement a function create_user(...) that supports the following semantics:
+create_user("John", "Doe") == \
+   {
+       "name": "John",
+       "surname": "Doe",
+       "age": 42 ,
+       "extra": {}
+   }
+
+create_user("Bill", "Gates", age=65) == \
+   {
+       "name": "Bill",
+       "surname": "Gates",
+       "age": 65,
+       "extra": {}
+   }
+
+create_user("Marie", "Curie", age=66, occupation="physicist",
+            won_nobel=True) == \
+   {
+       "name": "Marie",
+       "surname": "Curie",
+       "age": 66,
+       "extra": {
+           "occupation": "physicist",
+           "won_nobel": True
+       }
+   }
 """
 
 
